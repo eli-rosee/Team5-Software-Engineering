6b993a04300c7fd1ebbc72ed2bff4bacbc14baa5
braydencampbell123 <81635826+braydencampbell123@users.noreply.github.com>
2025-03-11 21:59:40 -0500

    Update splash.py

--- splash.py
+++ splash.py
@@ -5,6 +5,7 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setGeometry(700, 300, 500, 500)
         self.setStyleSheet("background-color: black;")
 
@@ -12,13 +13,10 @@ class MainWindow(QMainWindow):
         self.pixmap = QPixmap("graphics/logo.jpg")
         self.label.setPixmap(self.pixmap)
 
-        # Ensure the image scales with the label
         self.label.setScaledContents(True)
-
         self.showMaximized()
 
     def resizeEvent(self, event):
-        """Adjust label size when window is resized."""
         self.label.setGeometry(0, 0, self.width(), self.height())
         event.accept()
 
