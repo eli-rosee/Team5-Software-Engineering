2c20e1a836fc5cbb4228e146a1f822a7b0a8ad87
braydencampbell123 <81635826+braydencampbell123@users.noreply.github.com>
2025-04-14 00:45:15 -0500

    Update play_action_screen.py

--- play_action_screen.py
+++ play_action_screen.py
@@ -1,18 +1,30 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QPushButton, QApplication
-from PyQt6.QtCore import QTimer, Qt
+from PyQt6.QtCore import QTimer, Qt, pyqtSignal
 import sys
 import random
 import threading
 import time
 
-
-
 class PlayActionScreen(QWidget):
+    action_signal = pyqtSignal(str)
+    score_signal = pyqtSignal(str, str)  
+    team_score_signal = pyqtSignal(str)
+
     def __init__(self, red_players, green_players, photon_network, player_entry_screen_instance, parent=None):
         super().__init__(parent)
+        self.action_signal.connect(self.append_to_current_action)
+        self.score_signal.connect(self.change_player_score)
+        self.team_score_signal.connect(self.change_team_score)
+        self.red_player_scores = {player_id: 0 for player_id, _, _ in red_players}
+        self.green_player_scores = {player_id: 0 for player_id, _, _ in green_players}
+        self.flash_timer = QTimer(self)
+        self.flash_timer.timeout.connect(self.flash_high_team_score)
+        self.flash_state = True
+        self.flash_timer.start(500)
         self.setWindowTitle("Play Action Screen")
         self.showMaximized()
         self.setStyleSheet("background-color: black; color: white;")
+        
 
         # Store players with their equipment IDs
         self.red_players = red_players
@@ -25,25 +37,41 @@ class PlayActionScreen(QWidget):
 
         main_layout = QHBoxLayout()
 
-        # Red team layout
         red_team_layout = QVBoxLayout()
         red_team_label = QLabel("RED TEAM")
+        red_team_score = QLabel("0")
         red_team_label.setStyleSheet("font-size: 20px; font-weight: bold; color: red;")
         red_team_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        red_team_score.setStyleSheet("font-size: 50px; font-weight: bold; color: red;")
+        red_team_score.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         red_team_layout.addWidget(red_team_label)
+        red_team_layout.addWidget(red_team_score)
 
-        # Add Red Team players
-        for player_id, player_name, equip_id in red_players:
-            player_label = QLabel(f"{player_id} - {player_name} (Equipment: {equip_id})")
+        self.red_team_score_label = red_team_score
+        self.red_team_score = 0
+
+        self.red_player_layout = QVBoxLayout()
+        red_players_count = len(red_players)
+        for i, (player_id, player_name, equip_id) in enumerate(red_players):
+            score = 0
+
+            if i < red_players_count - 1 or i == 0:
+               self.red_player_layout.addStretch(1)
+    
+            player_label = QLabel(f"{player_id} - {player_name} (Equipment: {equip_id}) Score: {score}")
             player_label.setStyleSheet("font-size: 16px; color: white;")
-            player_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)  
-            red_team_layout.addWidget(player_label)
+            player_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+            self.red_player_layout.addWidget(player_label)
 
             self.red_player_labels[f'{player_id}'] = player_label
-            print(self.red_player_labels)
 
+        red_team_layout.addLayout(self.red_player_layout)
 
-        main_layout.addLayout(red_team_layout, stretch=1)
+        red_team_layout.addStretch()
+
+        main_layout.addLayout(red_team_layout)
+        self.setLayout(main_layout)
 
         current_action_layout = QVBoxLayout()
 
@@ -71,25 +99,40 @@ class PlayActionScreen(QWidget):
 
         main_layout.addLayout(current_action_layout, stretch=1)
 
-        # Green team layout
         green_team_layout = QVBoxLayout()
         green_team_label = QLabel("GREEN TEAM")
+        green_team_score = QLabel("0")
         green_team_label.setStyleSheet("font-size: 20px; font-weight: bold; color: green;")
         green_team_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        green_team_score.setStyleSheet("font-size: 50px; font-weight: bold; color: green;")
+        green_team_score.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         green_team_layout.addWidget(green_team_label)
+        green_team_layout.addWidget(green_team_score)
 
-        # Add Green Team players 
-        for player_id, player_name, equip_id in green_players:
-            player_label = QLabel(f"{player_id} - {player_name} (Equipment: {equip_id})")
+        self.green_team_score_label = green_team_score
+        self.green_team_score = 0
+
+        self.green_player_layout = QVBoxLayout()
+        green_players_count = len(green_players)
+        for i, (player_id, player_name, equip_id) in enumerate(green_players):
+            score = 0
+
+            if i < green_players_count - 1 or i == 0:
+               self.green_player_layout.addStretch(1)
+    
+            player_label = QLabel(f"{player_id} - {player_name} (Equipment: {equip_id}) Score: {score}")
             player_label.setStyleSheet("font-size: 16px; color: white;")
-            player_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)  
-            green_team_layout.addWidget(player_label)
+            player_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+            self.green_player_layout.addWidget(player_label)
 
             self.green_player_labels[f'{player_id}'] = player_label
-            print(self.green_player_labels)
 
-        main_layout.addLayout(green_team_layout, stretch=1)
+        green_team_layout.addLayout(self.green_player_layout)
 
+        green_team_layout.addStretch()
+
+        main_layout.addLayout(green_team_layout)
         self.setLayout(main_layout)
 
         # Initialize and start game timer (6 minutes)
@@ -135,12 +178,21 @@ class PlayActionScreen(QWidget):
                 if random.randint(1, 2) == 1:
                     action_text = f"{red_player[1]} hit {green_player[1]}"
                     equipment_code = f"{red_equip_id}:{green_equip_id}"
+                    equip_parts = equipment_code.split(":")
+                    if len(equip_parts) == 2:
+                        attacker_id, target_id = equip_parts
+                        self.score_signal.emit(attacker_id, target_id)
                 else:
                     action_text = f"{green_player[1]} hit {red_player[1]}"
                     equipment_code = f"{green_equip_id}:{red_equip_id}"
+                    equip_parts = equipment_code.split(":")
+                    if len(equip_parts) == 2:
+                        attacker_id, target_id = equip_parts
+                        self.score_signal.emit(attacker_id, target_id)
 
                 centered_text = f"<div style='text-align: center;'>{action_text}</div>"
-                self.append_to_current_action(centered_text)
+                self.action_signal.emit(centered_text)
+
 
                 if self.photon_network:
                     self.photon_network.equipID(equipment_code)  # Broadcast equipment code to server
@@ -151,7 +203,8 @@ class PlayActionScreen(QWidget):
                 if counter == 10:
                     base_hit_text = f"{red_player[1]} hit the base!"
                     centered_base_hit_text = f"<div style='text-align: center;'>{base_hit_text}</div>"
-                    self.append_to_current_action(centered_base_hit_text)
+                    self.action_signal.emit(centered_base_hit_text)
+                    self.team_score_signal.emit(base_hit_text)
                     if self.photon_network:
                         self.photon_network.equipID(f"{red_equip_id}:43")  # Red team base hit
                     else:
@@ -159,7 +212,8 @@ class PlayActionScreen(QWidget):
                 elif counter == 20:
                     base_hit_text = f"{green_player[1]} hit the base!"
                     centered_base_hit_text = f"<div style='text-align: center;'>{base_hit_text}</div>"
-                    self.append_to_current_action(centered_base_hit_text)
+                    self.action_signal.emit(centered_base_hit_text)
+                    self.team_score_signal.emit(base_hit_text)
                     if self.photon_network:
                         self.photon_network.equipID(f"{red_equip_id}:53")  # Red team base hit
                     else:
@@ -181,6 +235,152 @@ class PlayActionScreen(QWidget):
         cursor.movePosition(cursor.MoveOperation.End)  
         self.current_action_text.setTextCursor(cursor)  
 
+    def change_team_score(self, text):
+        player_name = text.split(" hit")[0].strip()
+
+        red_names = [name for _, name, _ in self.red_players]
+        green_names = [name for _, name, _ in self.green_players]
+
+        if player_name in red_names:
+            self.red_team_score += 100
+            self.red_team_score_label.setText(str(self.red_team_score))
+            print(f"Updated Red Team Score: {self.red_team_score}")
+
+            for player_id, name, equip_id in self.red_players:
+                if name == player_name:
+                    self.red_player_scores[player_id] += 100
+                    label = self.red_player_labels.get(player_id)
+                    if label:
+                        label.setText(f"{player_id} - {name} (Equipment: {equip_id}) Score: {self.red_player_scores[player_id]}")
+                    break
+
+        elif player_name in green_names:
+            self.green_team_score += 100
+            self.green_team_score_label.setText(str(self.green_team_score))
+            print(f"Updated Green Team Score: {self.green_team_score}")
+
+            # Award 100 points to individual player
+            for player_id, name, equip_id in self.green_players:
+                if name == player_name:
+                    self.green_player_scores[player_id] += 100
+                    label = self.green_player_labels.get(player_id)
+                    if label:
+                        label.setText(f"{player_id} - {name} (Equipment: {equip_id}) Score: {self.green_player_scores[player_id]}")
+                    break
+        else:
+            print("ERROR: Player not found in either team.")
+
+        self.sort_players()
+
+               
+
+    def change_player_score(self, attacker_equip_id, target_equip_id):
+        attacker = None
+        target = None
+        attacker_team = None
+        target_team = None
+
+        for player in self.red_players:
+            if player[2] == attacker_equip_id:
+                attacker = player
+                attacker_team = "red"
+        for player in self.green_players:
+            if player[2] == attacker_equip_id:
+                attacker = player
+                attacker_team = "green"
+        for player in self.red_players + self.green_players:
+            if player[2] == target_equip_id:
+                target = player
+                target_team = "red" if player in self.red_players else "green"
+
+        if not attacker:
+            print(f"Unknown attacker with equip ID {attacker_equip_id}")
+            return
+
+        if target_equip_id in ("43", "53"):
+            score = 100
+            label_dict = self.red_player_labels if attacker_team == "red" else self.green_player_labels
+            score_dict = self.red_player_scores if attacker_team == "red" else self.green_player_scores
+
+            player_id = attacker[0]
+            name = attacker[1]
+            label = label_dict.get(player_id)
+
+            if label:
+                if not name.startswith("🅱"):
+                    name = f"🅱 {name}"
+                score_dict[player_id] += score
+                label.setText(f"{player_id} - {name} (Equipment: {attacker[2]}) Score: {score_dict[player_id]}")
+            return
+
+        if attacker_team and target_team:
+            label_dict = self.red_player_labels if attacker_team == "red" else self.green_player_labels
+            score_dict = self.red_player_scores if attacker_team == "red" else self.green_player_scores
+
+            player_id = attacker[0]
+            name = attacker[1]
+            label = label_dict.get(player_id)
+
+            if attacker_team == target_team:
+                score_change = -10  
+            else:
+                score_change = 10   
+
+            score_dict[player_id] += score_change
+
+            self.sort_players()
+
+            if label:
+                label.setText(f"{player_id} - {name} (Equipment: {attacker[2]}) Score: {score_dict[player_id]}")
+
+            if attacker_team == "red":
+                self.red_team_score += score_change
+                self.red_team_score_label.setText(str(self.red_team_score))
+                print(f"Updated Red Team Score: {self.red_team_score}")
+            else:
+                self.green_team_score += score_change
+                self.green_team_score_label.setText(str(self.green_team_score))
+                print(f"Updated Green Team Score: {self.green_team_score}")
+
+    def sort_players(self):
+        red_sorted = sorted(self.red_players, key=lambda p: self.red_player_scores.get(p[0], 0), reverse=True)
+        for i in reversed(range(self.red_player_layout.count())):
+            widget = self.red_player_layout.itemAt(i).widget()
+            if widget:
+                widget.setParent(None)
+        for player_id, _, _ in red_sorted:
+            self.red_player_layout.addWidget(self.red_player_labels[player_id])
+        self.red_player_layout.addStretch()
+
+        green_sorted = sorted(self.green_players, key=lambda p: self.green_player_scores.get(p[0], 0), reverse=True)
+        for i in reversed(range(self.green_player_layout.count())):
+            widget = self.green_player_layout.itemAt(i).widget()
+            if widget:
+                widget.setParent(None)
+        for player_id, _, _ in green_sorted:
+            self.green_player_layout.addWidget(self.green_player_labels[player_id])
+        self.green_player_layout.addStretch()
+
+    def flash_high_team_score(self):
+        if self.red_team_score > self.green_team_score:
+            self.red_team_score_label.setStyleSheet(
+                f"font-size: 50px; font-weight: bold; color: {'red' if self.flash_state else 'black'};"
+            )
+            self.green_team_score_label.setStyleSheet("font-size: 50px; font-weight: bold; color: green;")
+        elif self.green_team_score > self.red_team_score:
+            self.green_team_score_label.setStyleSheet(
+                f"font-size: 50px; font-weight: bold; color: {'green' if self.flash_state else 'black'};"
+            )
+            self.red_team_score_label.setStyleSheet("font-size: 50px; font-weight: bold; color: red;")
+        else:
+            # Tie: both show normally
+            self.red_team_score_label.setStyleSheet("font-size: 50px; font-weight: bold; color: red;")
+            self.green_team_score_label.setStyleSheet("font-size: 50px; font-weight: bold; color: green;")
+        
+        self.flash_state = not self.flash_state
+
+
+
     def return_to_entry_screen(self):
         """Stop the traffic generator and return to the player entry screen."""
         from player_entry_screen import PlayerEntryScreen  
@@ -197,8 +397,8 @@ class PlayActionScreen(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    red_players = [("6005", "Scooby Deo Mt Opus", "111"), ("5000", "Euryum's Euryus Auxilus", "112")]
-    green_players = [("6005", "Scooby Deo Mt Opus", "221"), ("5000", "Opus Mt Scooby Deo", "222")]
-    screen = PlayActionScreen(red_players, green_players, None)
+    red_players = [("6005", "Player 1", "111"), ("5000", "Player 2", "112")]
+    green_players = [("6005", "Player 3", "221"), ("5000", "Player 4", "222")]
+    screen = PlayActionScreen(red_players, green_players, None, None)
     screen.show()
     sys.exit(app.exec())
